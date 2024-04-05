7f21faca68771f5720048b5d133ca07f1a5c653e
galeonn321 <santichiste@hotmail.com>
2024-04-05 20:36:00 +0200

    final commit

---
+++ components/about.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        In 2019, I graduated from my bachelor's program in Professional Language
+        Use with a specialization in English at Stockholm University, Sweden.
+        Aside from Linguistics, I learned about English for specific purposes,
+        intercultural and{" "}
+        <span className="font-bold">technical communication</span>, language in
+        professional settings, rhetoric, among others. In 2021, I finished my
+        master's studies in Linguistics at Leiden University, Netherlands, where
+        I dived further into the world of language and communication.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not writing professionally</span>, I
+        enjoy playing I enjoy crafting and writing stories in my spare time ,
+        watching films, reading, walking, and spinning. I also enjoy learning
+        new things. At the moment, I am learning new languages and new skills.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,131 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import sample1 from "@/public/samples/sample_1_image.png";
+import sample2 from "@/public/samples/sample_2_image.png";
+import sample3 from "@/public/samples/sample_3_image.png";
+import sample4 from "@/public/samples/sample_4_image.png";
+import sample5 from "@/public/samples/sample_5_image.png";
+import sample6 from "@/public/samples/sample_6_image.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Technical Content Specialist - Accell Group",
+    location: "Amsterdam, North Holland, Netherlands",
+    description: `Own, maintain, update, and optimise the internal user knowledge base (Confluence) on systems, processes, and new features in collaboration with the development, UX and SEO Teams. Provide support to the company brands in terms of training, translation, proofreading, content management, and best practices. 
+    Help revise best practices, scalability, and omnichannel ways to manage content on the systems used for e-commerce and content management.`,
+    icon: React.createElement(CgWorkAlt),
+    date: "Feb 2021 - Present",
+  },
+  {
+    title: "Junior Content Specialist - Accell Group",
+    location: "Amsterdam, North Holland, Netherlands",
+    description:
+      "Revise, edit, and proofread editorial and product content for multiple countries and languages. Assist in content migration projects. Assist in the management of editorial and product content.Write translation guidelines and terminology lists for Spanish and English.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
+  },
+  {
+    title: "English - Spanish Translator - Natuurhuisje",
+    location: "Breda, Netherlands",
+    description:
+      "Translation and editing of the company’s website and general digital documents. SEO content writing for the website's Spanish version.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Dec 2019 - Feb 2020",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "How are product number URLs managed?",
+    description:
+      "This is a guide that explains how product URLs are sluggified in a system I helped document.",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample1,
+    document:'/samples/Documentation-sample-1-Catalina-Maruri-Ramos.pdf'
+  },
+  {
+    title: "Manage custom calendars",
+    description:
+      "This is a how-to guide that explains how to manage custom calendars for a system I helped document.",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample2,
+    document:'/samples/Documentation-sample-2-Catalina-Maruri-Ramos.pdf'
+  },
+  {
+    title: "Image file types and formats",
+    description:
+      "This is a guide that explains the different image file types and formats used for a system I helped document",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample3,
+    document:'/samples/Documentation-sample-3-Catalina-Maruri-Ramos.pdf'
+  },
+  {
+    title: "How to set focal point on images",
+    description:
+      "This is a guide that explains the different image file types and formats used for a system I helped document.",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample4,
+    document:'/samples/Documentation-sample-4-Catalina-Maruri-Ramos.pdf'
+  },
+  {
+    title: "Enable the newsletter pop-up",
+    description:
+      "This is a how-to guide that explains how to enable newsletter pop ups on sites through a system I helped document",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample5,
+    document:'/samples/Documentation-sample-5-Catalina-Maruri-Ramos.pdf'
+  },
+  {
+    title: "Deploy and publish content",
+    description:
+      "This is a guide that explains how to publish content live through a system I helped document.",
+    tags: ["SaaS", "Confluence", "CMS"],
+    imageUrl: sample6,
+    document:'/samples/Documentation-sample-6-Catalina-Maruri-Ramos.pdf'
+  },
+] as const;
+
+export const skillsData = [
+  "Excellent Writing Skills",
+  "Technical Aptitude",
+  "Research Skills",
+  "Attention to Detail",
+  "Audience Awareness",
+  "Organizational Skills",
+  "Collaboration Skills",
+  "Project Management",
+  "Knowledge of Tools",
+  "Visual Communication",
+  "Adaptability",
+  "Problem-Solving Skills",
+  "Continuous Learning",
+  "Empathy",
+  "Quality Assurance",
+] as const;
